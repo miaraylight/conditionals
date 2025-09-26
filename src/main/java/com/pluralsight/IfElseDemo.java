618ee74baae1dac6954777808a2c72761f485f4f
miaraylight <miaraylight@gmail.com>
2025-09-26 16:09:40 -0500

    step 4-5

--- src/main/java/com/pluralsight/IfElseDemo.java
+++ src/main/java/com/pluralsight/IfElseDemo.java
@@ -33,6 +33,39 @@ public class IfElseDemo {
         } else if (grade.equals("F")) {
             System.out.println("Failed.");
         }
+
+
+
+        // Example: Compare strings using == and .equals()
+        String word1 = "hello";
+        String word2 = new String("hello");
+
+
+        System.out.println(word1 == word2);
+
+        System.out.println(word1.equals((word2)));
+
+        // Observe the difference. Which one is correct for comparing string content?
+        // o_O
+
+
+        // Ask for username
+        System.out.print("Enter username: ");
+        String username = scanner.next();
+
+        // Ask for password
+        System.out.print("Enter password: ");
+        String password = scanner.next();
+
+        if (username.equals("Admin") && password.equals("1234")) {
+            System.out.println("Access granted.");
+        } else {
+            System.out.println("Access denied.");
+        }
     }
+
+
+
+
 }
 
