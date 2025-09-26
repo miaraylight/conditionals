7d13bc099252db251cdf8f9cfade2fa93507fabb
miaraylight <miaraylight@gmail.com>
2025-09-26 15:58:53 -0500

    step 3

--- src/main/java/com/pluralsight/IfElseDemo.java
+++ src/main/java/com/pluralsight/IfElseDemo.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class IfElseDemo {
@@ -10,12 +11,27 @@ public class IfElseDemo {
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
-        // TODO: Write an if statement that checks if age is >= 18
-        // If true, print "You are an adult."
         if (age >= 18) {
             System.out.println("You are an adult.");
         } else {
             System.out.println("You are not an adult yet.");
+        }
+
+        // STEP 3
+
+        System.out.println("Enter your grade (A-F):");
+        String grade = scanner.next();
+
+        if (grade.equals("A")) {
+            System.out.println("Excellent!");
+        } else if (grade.equals(("B"))) {
+            System.out.println("Good job!");
+        } else if (grade.equals("C")) {
+            System.out.println("You passed.");
+        } else if (grade.equals("D")) {
+            System.out.println("Barely passed.");
+        } else if (grade.equals("F")) {
+            System.out.println("Failed.");
         }
     }
 }
